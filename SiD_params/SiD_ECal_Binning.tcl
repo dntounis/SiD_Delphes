6f72b8d6ca882071f7e2c8055c8b54301730ae49
Dimitrios Ntounis <dimitris.ntounis@gmail.com>
2024-11-03 17:23:50 -0800

    add new binning for SiD

--- SiD_params/SiD_ECal_Binning.tcl
+++ SiD_params/SiD_ECal_Binning.tcl
@@ -1,18 +1,29 @@
   set pi [expr {acos(-1)}]
 
-
   # lists of the edges of each tower in eta and phi
   # each list starts with the lower edge of the first tower
   # the list ends with the higher edge of the last tower
 
-  # 0.5 degree towers (5x5 mm^2)
+  # Cell size: assume 50 μm x 50 μm MAPS up to eta max = 2.5
+  # ECal Barrel : rinner = 126.5 cm, router = 140.9cm --> r_mid = 133.7 cm
+
+
+  # DeltaEtaPhi = cell_size / r_mid = 50e-6 / 133.7e-2 = 0.00004
+
+  # Number of bins in eta = 2*2.5 / 0.00004 = 125,000
+  # Number of bins in phi = 2*pi / (0.00004*cosh(1.25)) ~ 66,000 (1.25 == eta_mid)
+
+  set EtaPhiRes 0.00004
+  # number of bins in each side (half of the total!!!)
+  set nbins_phi 33000  
+  set nbins_eta 62500
+
   set PhiBins {}
-  for {set i -360} {$i <= 360} {incr i} {
-    add PhiBins [expr {$i * $pi/360.0}]
+  for {set i -$nbins_phi} {$i <= $nbins_phi} {incr i} {
+    add PhiBins [expr {$i * $pi/$nbins_phi}]
   }
 
-  # 0.01 unit in eta up to eta = 2.5
-  for {set i -500} {$i <= 500} {incr i} {
-    set eta [expr {$i * 0.005}]
+  for {set i -$nbins_eta} {$i <= $nbins_eta} {incr i} {
+    set eta [expr {$i * $EtaPhiRes}]
     add EtaPhiBins $eta $PhiBins
   }
--- SiD_params/SiD_HCal_Binning.tcl
+++ SiD_params/SiD_HCal_Binning.tcl
@@ -1,17 +1,31 @@
-  set pi [expr {acos(-1)}]
+ set pi [expr {acos(-1)}]
 
   # lists of the edges of each tower in eta and phi
   # each list starts with the lower edge of the first tower
-  # the list ends with the higher edged of the last tower
+  # the list ends with the higher edge of the last tower
 
-  # 6 degree towers
+  # Cell size: assume 3 cm x 3 cm  up to eta max = 3
+  # ECal Barrel : rinner = 141.7 cm, router = 249.3 cm --> r_mid = 195.5 cm
+
+
+  # DeltaEtaPhi = cell_size / r_mid = 3e-2 / 195.5e-2 = 0.015
+
+  # Number of bins in eta = 2*3.0 / 0.015 = 400
+  # Number of bins in phi = 2*pi / (0.015*cosh(1.5)) ~ 180
+
+  set EtaPhiRes 0.015
+
+  # number of bins in each side (half of the total!!!)
+  set nbins_phi 200  
+  set nbins_eta 90
+
+
   set PhiBins {}
-  for {set i -60} {$i <= 60} {incr i} {
-    add PhiBins [expr {$i * $pi/60.0}]
+  for {set i -$nbins_phi} {$i <= $nbins_phi} {incr i} {
+    add PhiBins [expr {$i * $pi/$nbins_phi}]
   }
 
-  # 0.5 unit in eta up to eta = 3
-  for {set i -60} {$i <= 60} {incr i} {
-    set eta [expr {$i * 0.05}]
+  for {set i -$nbins_eta} {$i <= $nbins_eta} {incr i} {
+    set eta [expr {$i * $EtaPhiRes}]
     add EtaPhiBins $eta $PhiBins
   }
